4b757bccc69df61f784fadc5c181fb958ebb6a08
Luke Barr <lukebarr@outlook.com>
2019-02-18 18:53:37 -0600

    Adds no preview scripts

--- scripts/6) train LIAEF128 (no preview).sh
+++ scripts/6) train LIAEF128 (no preview).sh
@@ -5,4 +5,6 @@ python "$DFL_SRC/main.py" train \
     --training-data-src-dir "$DFL_WORKSPACE/data_src/aligned" \
     --training-data-dst-dir "$DFL_WORKSPACE/data_dst/aligned" \
     --model-dir "$DFL_WORKSPACE/model" \
-    --model SAE
+    --model LIAEF128 \
+    --disable-preview
+
--- scripts/6) train SAE (no preview).sh
+++ scripts/6) train SAE (no preview).sh
@@ -5,4 +5,5 @@ python "$DFL_SRC/main.py" train \
     --training-data-src-dir "$DFL_WORKSPACE/data_src/aligned" \
     --training-data-dst-dir "$DFL_WORKSPACE/data_dst/aligned" \
     --model-dir "$DFL_WORKSPACE/model" \
-    --model SAE
+    --model SAE \
+    --disable-preview
